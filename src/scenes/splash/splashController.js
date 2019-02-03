987e927e471990845dcd52086aa5135b3c244c22
marcosOrdieres <marcosordieres89@gmail.com>
2019-02-03 17:19:56 +0100

    main funct game over

--- src/scenes/splash/splashController.js
+++ src/scenes/splash/splashController.js
@@ -1,7 +1,8 @@
 import { BaseScene } from 'components';
 import template from './splashTemplate';
 import { connect } from 'react-redux';
-import { Dimensions } from 'react-native';
+import { Dimensions, ToastAndroid } from 'react-native';
+import { StackActions, NavigationActions } from 'react-navigation';
 
 const {width, height} = Dimensions.get('window');
 
@@ -12,7 +13,9 @@ class SplashController extends BaseScene {
       renderAgain: false,
       numberFaces: [0],
       top: (Math.floor((Math.random() * (height / 2 - 0)) + 0)),
-      left: (Math.floor((Math.random() * (width / 3 - 0)) + 0))
+      left: (Math.floor((Math.random() * (width / 3 - 0)) + 0)),
+      toastVisible: false,
+      buttonGameOver: false
     };
   }
 
@@ -29,6 +32,24 @@ class SplashController extends BaseScene {
     });
     console.log(topArray, leftArray);
     return {topArray, leftArray};
+  }
+
+  gameOver () {
+    ToastAndroid.showWithGravityAndOffset(
+        'GAME OVER',
+        ToastAndroid.LONG,
+        ToastAndroid.CENTER,
+        0,
+        100
+      );
+    this.setState({buttonGameOver: true});
+  }
+
+  onStartAgain () {
+    this.setState({
+      buttonGameOver: false,
+      numberFaces: [0]
+    });
   }
 
   render () {
--- src/scenes/splash/splashTemplate.js
+++ src/scenes/splash/splashTemplate.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Dimensions, TouchableOpacity } from 'react-native';
+import { View, Text, Dimensions, TouchableOpacity, ToastAndroid, Button } from 'react-native';
 import logo from '../../images/logoMaps.png';
 import splashStyles from './splashStyles';
 import TimerCountdown from 'react-native-timer-countdown';
@@ -11,69 +11,91 @@ import TimerCountdown from 'react-native-timer-countdown';
   allowFontScaling
   style={{ fontSize: 20 }}
 /> */
+
+// firebase, admob, button comenzar otra vez,
+
 const {width, height} = Dimensions.get('window');
 
 export default (controller) => (
-
-  <View style={{
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'stretch'
-  }}>
-    <TouchableOpacity
-      onPress={() => { console.log('GAME OVER'); }}
-      style={{width: width / 2, backgroundColor: 'powderblue'}}>
+  <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'stretch'}}>
+    {!controller.state.buttonGameOver ?
       <TouchableOpacity
-        onPress={() => {
-          console.log('CORRECTTTTTT');
-          let newelement = [controller.state.numberFaces.slice(-1)[0] + 1, controller.state.numberFaces.slice(-1)[0] + 2];
-          let joined = controller.state.numberFaces.concat(newelement);
-          controller.setState({
-            numberFaces: joined
-          });
-          const topAndLeft = controller.setTopAndLeft();
-          controller.user.setTopStyle(topAndLeft.topArray);
-          controller.user.setLeftStyle(topAndLeft.leftArray);
-        }}
+        onPress={() => { controller.gameOver(); }}
+        style={{width: width / 2, backgroundColor: 'powderblue'}}>
+        <TouchableOpacity
+          onPress={() => {
+            console.log('CORRECTTTTTT');
+            let newelement = [controller.state.numberFaces.slice(-1)[0] + 1, controller.state.numberFaces.slice(-1)[0] + 2];
+            let joined = controller.state.numberFaces.concat(newelement);
+            controller.setState({
+              numberFaces: joined
+            });
+            const topAndLeft = controller.setTopAndLeft();
+            controller.user.setTopStyle(topAndLeft.topArray);
+            controller.user.setLeftStyle(topAndLeft.leftArray);
+          }}
+          style={{
+            backgroundColor: 'red',
+            width: 50,
+            height: 50,
+            borderRadius: 50 / 2,
+            position: 'absolute',
+            top: (Math.floor((Math.random() * (height / 2 - 0)) + 0)),
+            left: (Math.floor((Math.random() * (width / 3 - 0)) + 0))
+          }} />
+        {controller.generateFaces().map((face, index) =>
+          <TouchableOpacity
+            onPress={() => { controller.gameOver(); }}
+            style={{
+              backgroundColor: 'blue',
+              width: 50,
+              height: 50,
+              borderRadius: 50 / 2,
+              position: 'relative',
+              top: !controller.user.getTopStyle() ? controller.state.top : controller.user.getTopStyle()[face],
+              left: !controller.user.getLeftStyle() ? controller.state.left : controller.user.getLeftStyle()[face]
+            }} />
+        )}
+      </TouchableOpacity>
+      :
+      null
+    }
+    {controller.state.buttonGameOver ?
+      <TouchableOpacity
         style={{
+          width: 200,
+          height: 200,
+          borderRadius: 100 / 2,
           backgroundColor: 'red',
-          width: 50,
-          height: 50,
-          borderRadius: 50 / 2,
-          position: 'absolute',
-          top: (Math.floor((Math.random() * (height / 2 - 0)) + 0)),
-          left: (Math.floor((Math.random() * (width / 3 - 0)) + 0))
-        }} />
-      {controller.generateFaces().map((face, index) =>
-        <TouchableOpacity
-          onPress={() => { console.log('GAME OVER IN THE LEFT'); }}
-          style={{
-            backgroundColor: 'blue',
-            width: 50,
-            height: 50,
-            borderRadius: 50 / 2,
-            position: 'relative',
-            top: !controller.user.getTopStyle() ? controller.state.top : controller.user.getTopStyle()[face],
-            left: !controller.user.getLeftStyle() ? controller.state.left : controller.user.getLeftStyle()[face]
-          }} />
-      )}
-    </TouchableOpacity>
-    <TouchableOpacity style={{width: width / 2, backgroundColor: 'skyblue'}}>
-      {controller.generateFaces().map((face, index) =>
-        <TouchableOpacity
-          onPress={() => { console.log('GAME OVER IN THE RIGHT'); }}
-          style={{
-            backgroundColor: 'blue',
-            width: 50,
-            height: 50,
-            borderRadius: 50 / 2,
-            position: 'relative',
-            top: !controller.user.getTopStyle() ? controller.state.top : controller.user.getTopStyle()[face],
-            left: !controller.user.getLeftStyle() ? controller.state.left : controller.user.getLeftStyle()[face]
-          }} />
-    )}
-    </TouchableOpacity>
-
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderColor: 'black',
+          marginTop: '20%',
+          borderWidth: 5}}
+        onPress={() => { controller.onStartAgain(); }}>
+        <Text style={{color: 'white', textAlign: 'center', fontWeight: 'bold', fontSize: 18}}>Try Again!</Text>
+      </TouchableOpacity>
+    :
+    null
+    }
+    {!controller.state.buttonGameOver ?
+      <TouchableOpacity style={{width: width / 2, backgroundColor: 'skyblue'}}>
+        {controller.generateFaces().map((face, index) =>
+          <TouchableOpacity
+            onPress={() => { controller.gameOver(); }}
+            style={{
+              backgroundColor: 'blue',
+              width: 50,
+              height: 50,
+              borderRadius: 50 / 2,
+              position: 'relative',
+              top: !controller.user.getTopStyle() ? controller.state.top : controller.user.getTopStyle()[face],
+              left: !controller.user.getLeftStyle() ? controller.state.left : controller.user.getLeftStyle()[face]
+            }} />
+          )}
+      </TouchableOpacity>
+      :
+      null
+    }
   </View>
 );
