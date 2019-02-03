b0da25350ff51f43c1de93b78390db2fcd9c7009
marcosOrdieres <marcosordieres89@gmail.com>
2019-02-03 21:42:22 +0100

    trump and putin

--- src/scenes/splash/splashTemplate.js
+++ src/scenes/splash/splashTemplate.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Dimensions, TouchableOpacity, ToastAndroid, Button } from 'react-native';
+import { View, Text, Dimensions, TouchableOpacity, ToastAndroid, Button, Image } from 'react-native';
 import logo from '../../images/logoMaps.png';
 import splashStyles from './splashStyles';
 import TimerCountdown from 'react-native-timer-countdown';
@@ -35,13 +35,18 @@ export default (controller) => (
             const topAndLeft = controller.setTopAndLeft();
             controller.user.setTopStyle(topAndLeft.topArray);
             controller.user.setLeftStyle(topAndLeft.leftArray);
+            controller.chargeAd();
           }}
           style={{
+            zIndex: 1000,
             position: 'absolute',
             top: (Math.floor((Math.random() * (height / 2 - 0)) + 0)),
             left: (Math.floor((Math.random() * (width / 3 - 0)) + 0))
           }}>
-          <BombExplosion width={75} height={75} />
+          <Image
+            style={{width: 75, height: 75}}
+            source={require('../../assets/images/trump.png')} />
+          {/* <BombExplosion width={75} height={75} /> */}
         </TouchableOpacity>
         {controller.generateFaces().map((face, index) =>
           <TouchableOpacity
@@ -51,7 +56,11 @@ export default (controller) => (
               top: !controller.user.getTopStyle() ? controller.state.top : controller.user.getTopStyle()[face],
               left: !controller.user.getLeftStyle() ? controller.state.left : controller.user.getLeftStyle()[face]
             }}>
-            <BombSafe width={75} height={75} />
+            <Image
+              style={{width: 50, height: 62}}
+              source={require('../../assets/images/putin.png')} />
+
+            {/* <BombSafe width={75} height={75} /> */}
           </TouchableOpacity>
         )}
       </TouchableOpacity>
@@ -86,7 +95,11 @@ export default (controller) => (
               top: !controller.user.getTopStyle() ? controller.state.top : controller.user.getTopStyle()[face],
               left: !controller.user.getLeftStyle() ? controller.state.left : controller.user.getLeftStyle()[face]
             }}>
-            <BombSafe width={75} height={75} />
+            <Image
+              style={{width: 50, height: 62}}
+              source={require('../../assets/images/putin.png')} />
+
+            {/* <BombSafe width={75} height={75} /> */}
           </TouchableOpacity>
           )}
       </TouchableOpacity>
